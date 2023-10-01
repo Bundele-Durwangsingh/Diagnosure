5c880fe609b39d4ec06034a5e25a301105c099ce
DRAG-11 <bundeledurwang@gmail.com>
2023-10-01 21:28:25 +0530

    finalised the model for diabetes

--- Backend/Finished model/diabetes.py
+++ Backend/Finished model/diabetes.py
@@ -2,24 +2,16 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 
-df = pd.read_csv("D:\project\major project implementation\Data\survey lung cancer.csv")
-df.head()
+df = pd.read_csv("D:\project\Integrated Health Prognosis using Deep Learning\Data\diabetes_prediction_dataset.csv")
+df['gender']= df['gender'].apply({'Male':1, 'Female':0}.get)
+df['smoking_history']= df['smoking_history'].apply({'No Info':0,'never':1,'former':2,'not current':3,'ever':4,'current':5}.get)
+df['age'] = df['age'].astype(np.int64)
+df['gender'] = df['gender'].fillna(0).astype(np.int64)
 
-ds= pd.DataFrame(df)
-ds=ds.drop(['PEER_PRESSURE'], axis=1)
-ds.shape
-
-ds['GENDER']= ds['GENDER'].apply({'M':1, 'F':0}.get)
-ds.head(10)
-ds['LUNG_CANCER']= ds['LUNG_CANCER'].apply({'YES':1, 'NO':0}.get)
-ds.head()
-
-X = ds.drop('LUNG_CANCER', axis=1)
-y = ds['LUNG_CANCER']
-
+X = df.drop('diabetes', axis=1)
+y = df['diabetes']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=101)
 scaler = StandardScaler()
 scaled_X_train = scaler.fit_transform(X_train)
@@ -30,7 +22,7 @@ RF.fit(X_train, y_train)
 pred_rf = RF.predict(X_test)
 print("Accuracy score for Random Forest")
 print(RF.score(X_test,y_test))
-input_data = (0,59,1,1,1,1,2,1,2,1,2,2,1,2)
+input_data = (0,44,0,0,1,19.31,6.5,200)
 
 #change the input data to a numpy array
 input_data_as_numpy_array = np.asarray(input_data)
@@ -42,6 +34,6 @@ prediction = RF.predict(input_data_reshaped)
 print(prediction)
 
 if (prediction[0] == 0):
-    print('The Person does not have a lung cancer')
+    print('The Person does not have a diabetes')
 else:
-    print('The Person might have lung cancer')
+    print('The Person might have diabetes')
