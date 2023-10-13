be2cf8c5b317f2780fe5358a19c31d73c4584c86
DRAG-11 <bundeledurwang@gmail.com>
2023-10-13 22:04:08 +0530

    Started working on main app development

---
+++ Main/model.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/heart.html')
+def home():
+    return render_template('heart.html')
+
+
+@app.route('/hdp_predict', methods=['POST'])
+def predict():
+    age = int(request.form.get('age'))
+    sex = int(request.form.get('sex'))
+    cp = int(request.form.get("cp"))
+    trestbps = int(request.form.get("trestbps"))
+    chol = int(request.form.get("chol"))
+    fbs = int(request.form.get("fbs"))
+    restecg = int(request.form.get("restecg"))
+    thalach = int(request.form.get("thalach"))
+    exang = int(request.form.get("exang"))
+    oldpeak = int(request.form.get("oldpeak"))
+    slope = int(request.form.get("slope"))
+    ca = int(request.form.get("ca"))
+    thal = int(request.form.get("thal"))
+    input_data = (age, sex, cp, trestbps, chol, fbs, restecg,
+                  thalach, exang, oldpeak, slope, ca, thal)
+    input_data_as_numpy_array = np.asarray(input_data)
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+    prediction = model.predict(input_data_reshaped)
+    print(prediction)
+    if prediction == 1:
+        return render_template('heart.html', label=1)
+    else:
+        return render_template('heart.html', label=-1)
+
+@app.route('/lungs.html')
+def lcp():
+    return render_template('lungs.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- Main/test.py
+++ Main/test.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
 
-heart_data = pd.read_csv("D:\project\major project implementation\Data\heart_disease_data.csv")
+heart_data = pd.read_csv('heart_disease_data.csv')
 X = heart_data.drop(columns='target', axis=1)
 Y = heart_data['target']
 X_train, X_test, Y_train, Y_test = train_test_split(
@@ -22,23 +22,23 @@ training_data_accuracy = accuracy_score(X_train_prediction, Y_train)
 print('Accuracy on Training data : ', training_data_accuracy)
 print('Accuracy on Test data : ', test_data_accuracy)
 
-#pickle.dump(model,open('model.pkl','wb'))
+pickle.dump(model,open('model.pkl','wb'))
 
 
 
 
-input_data = (62, 0, 0, 140, 268, 0, 0, 160, 0, 3.6, 0, 2, 2)
+#input_data = (62, 0, 0, 140, 268, 0, 0, 160, 0, 3.6, 0, 2, 2)
 
- #change the input data to a numpy array
-input_data_as_numpy_array = np.asarray(input_data)
+# change the input data to a numpy array
+#input_data_as_numpy_array = np.asarray(input_data)
 
- #reshape the numpy array as we are predicting for only on instance
-input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+# reshape the numpy array as we are predicting for only on instance
+#input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
-prediction = model.predict(input_data_reshaped)
-print(prediction)
+#prediction = model.predict(input_data_reshaped)
+#print(prediction)
 
-if (prediction[0] == 0):
-    print('The Person does not have a Heart Disease')
-else:
-    print('The Person has Heart Disease')
+#if (prediction[0] == 0):
+#    print('The Person does not have a Heart Disease')
+#else:
+#    print('The Person has Heart Disease')
